40c6466472348cef35d1ad6fc4cb716cb0de5b34
Andrew Hummel <ahummel25@gmail.com>
2020-10-23 13:58:03 -0500

    Initial commit

---
+++ graphql/resolvers/resolver.go
@@ -0,0 +1,25 @@
+package resolvers
+
+import (
+	"github.com/ahummel25/user-auth-api/graphql/generated"
+	"github.com/ahummel25/user-auth-api/graphql/resolvers/mutations"
+	"github.com/ahummel25/user-auth-api/graphql/resolvers/query"
+)
+
+// This file will not be regenerated automatically.
+//
+// It serves as dependency injection for your app, add any dependencies you require here.
+
+type Services struct {
+	mutations.MutationResolvers
+	query.QueryResolvers
+}
+
+type MutationResolver struct{ *Services }
+type QueryResolver struct{ *Services }
+
+// Mutation returns generated.MutationResolver implementation.
+func (r *Services) Mutation() generated.MutationResolver { return &MutationResolver{r} }
+
+// Query returns generated.QueryResolver implementation.
+func (r *Services) Query() generated.QueryResolver { return &QueryResolver{r} }
