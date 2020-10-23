557dd004e87fb21126413cbcd8e75d5950a6c678
Andrew Hummel <ahummel25@gmail.com>
2020-10-23 13:58:03 -0500

    Single commit for main

---
+++ graphql/resolvers/query/user/user.go
@@ -0,0 +1,17 @@
+package user
+
+import (
+	"context"
+
+	"github.com/src/user-auth-api/graphql/model"
+	"github.com/src/user-auth-api/service/user"
+)
+
+// Resolver contains the services that user query resolver calls into
+type Resolver struct {
+	UserService user.API
+}
+
+func (r *Resolver) AuthenticateUser(ctx context.Context, params model.AuthParams) (*model.UserObject, error) {
+	return r.UserService.AuthenticateUser(ctx, params.UsernameOrEmail, params.Password)
+}
