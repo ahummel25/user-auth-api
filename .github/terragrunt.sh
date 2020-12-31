3a93929d94d0f3e9be7da6eca2ce667a904449ed
Andrew Hummel <ahummel25@gmail.com>
2020-12-30 21:47:44 -0600

    Use env for terragrunt ci

--- .github/terragrunt.sh
+++ .github/terragrunt.sh
@@ -4,8 +4,15 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+ENV=$1
+
+if [ -z "$ENV" ]; then
+  echo "ENV variable is empty!\n"
+  exit 1
+fi
+
 terragrunt --version
 
-cd infra-live/non-prod/us-east-1/dev
+cd infra-live/non-prod/us-east-1/$ENV
 
 terragrunt plan-all
