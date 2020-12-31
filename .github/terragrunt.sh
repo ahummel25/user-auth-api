74289978fdbaf7794ac1e1292ab787f012449404
Andrew Hummel <ahummel25@gmail.com>
2020-12-30 22:09:39 -0600

    Pass env var

--- .github/terragrunt.sh
+++ .github/terragrunt.sh
@@ -5,10 +5,17 @@ set -euo pipefail
 IFS=$'\n\t'
 
 ENV=$1
-SUB_ENV=$2
+TERRAGRUNT_LIVE_DIR=""
+
+# If prod, no sub directory exists. If non-prod, it does.
+if [ $ENV == "prod" ]; then
+	TERRAGRUNT_LIVE_DIR="infra-live/$ENV/us-east-1"
+else
+    TERRAGRUNT_LIVE_DIR="infra-live/non-prod/us-east-1/$ENV"
+fi
 
 terragrunt --version
 
-cd infra-live/$ENV/us-east-1/$SUB_ENV
+cd $TERRAGRUNT_LIVE_DIR
 
 terragrunt plan-all
