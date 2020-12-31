6dc36f97904e77a2fadb3614d951775eab6fc8f3
Andrew Hummel <ahummel25@gmail.com>
2020-12-30 18:28:46 -0600

    Move role policy doc data to data file

--- infra-modules/iam/data.tf
+++ infra-modules/iam/data.tf
@@ -1,1 +1,13 @@
 data "aws_organizations_organization" "org" {}
+
+data "aws_iam_policy_document" "trust-assume-role-policy" {
+  statement {
+
+    actions = ["sts:AssumeRole", "sts:TagSession"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_organizations_organization.org.master_account_id}:root"]
+    }
+  }
+}
--- infra-modules/iam/main.tf
+++ infra-modules/iam/main.tf
@@ -1,15 +1,3 @@
-data "aws_iam_policy_document" "trust-assume-role-policy" {
-  statement {
-
-    actions = ["sts:AssumeRole", "sts:TagSession"]
-
-    principals {
-      type        = "AWS"
-      identifiers = ["arn:aws:iam::${data.aws_organizations_organization.org.master_account_id}:root"]
-    }
-  }
-}
-
 resource "aws_iam_role" "assumed_member_role" {
   name               = var.role_name
   assume_role_policy = data.aws_iam_policy_document.trust-assume-role-policy.json
