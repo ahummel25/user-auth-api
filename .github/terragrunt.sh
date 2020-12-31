a8315b6125c97306eff3a0faa5c72e88502a2e9a
Andrew Hummel <ahummel25@gmail.com>
2020-12-31 08:55:08 -0600

    Indentation fix

--- .github/terragrunt.sh
+++ .github/terragrunt.sh
@@ -9,7 +9,7 @@ TERRAGRUNT_LIVE_DIR=""
 
 # If prod, no sub directory exists. If non-prod, it does.
 if [ $ENV == "prod" ]; then
-	TERRAGRUNT_LIVE_DIR="infra-live/$ENV/us-east-1"
+    TERRAGRUNT_LIVE_DIR="infra-live/$ENV/us-east-1"
 else
     TERRAGRUNT_LIVE_DIR="infra-live/non-prod/us-east-1/$ENV"
 fi
