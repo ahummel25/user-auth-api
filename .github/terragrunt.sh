28f4d75faaea33f3097e9eacffab0f72a64eec24
Andrew Hummel <ahummel25@gmail.com>
2021-02-19 13:31:59 -0600

    Update shell script

--- .github/terragrunt.sh
+++ .github/terragrunt.sh
@@ -5,29 +5,31 @@ set -euo pipefail
 IFS=$'\n\t'
 
 ENV=$1
-TERRAGRUNT_LIVE_DIR=""
+TERRAGRUNT_LIVE_DIRS=()
 
 # If prod, no sub directory exists. If non-prod, it does.
 if [ $ENV == "prod" ]; then
-    TERRAGRUNT_LIVE_DIR="infra/aws-infra-live/$ENV/us-east-1"
+    TERRAGRUNT_LIVE_DIRS=("infra/aws-infra-live/$ENV/us-east-1" "infra/mongodb-infra-live/$ENV")
 else
-    TERRAGRUNT_LIVE_DIR="infra/aws-infra-live/non-prod/us-east-1/$ENV"
+    TERRAGRUNT_LIVE_DIRS=("infra/aws-infra-live/non-prod/us-east-1/$ENV" "infra/mongodb-infra-live/non-prod/$ENV")
 fi
 
 terragrunt --version
 
-cd $TERRAGRUNT_LIVE_DIR
+for TERRAGRUNT_LIVE_DIR in ${TERRAGRUNT_LIVE_DIRS[@]}; do
+    cd $GITHUB_WORKSPACE/$TERRAGRUNT_LIVE_DIR
 
-module_dirs=($(ls -d */))
+    module_dirs=($(ls -d */))
 
-for module in ${module_dirs[@]}; do
-    current_dir=$GITHUB_WORKSPACE/$TERRAGRUNT_LIVE_DIR/$module
-    cd $current_dir
-    echo -e "Running terragrunt plan for $current_dir\n"
+    for module in ${module_dirs[@]}; do
+        current_dir=$GITHUB_WORKSPACE/$TERRAGRUNT_LIVE_DIR/$module
+        cd $current_dir
+        echo -e "Running terragrunt plan for $current_dir\n"
 
-    terragrunt plan
-    echo -e "\n"
-    
-    #terragrunt apply --auto-approve
-    #echo -e "\n"
+        terragrunt plan
+        echo -e "\n"
+        
+        #terragrunt apply --auto-approve
+        #echo -e "\n"
+    done;
 done;
